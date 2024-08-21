87bfe476921b1e58ff48f605c7e12e278013da02
pneumatick <55963487+pneumatick@users.noreply.github.com>
2024-08-21 10:47:31 -0400

    Add timeline plot to script

---
+++ src/plots/timeline.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Import the dataset
+df = pd.DataFrame(pd.read_csv("assets/normalized_pyramid_data.csv"))
+
+tl = df.sort_values(by='start_of_reign', ascending=False)
+
+# Add rule time estimates for Ibi
+tl.loc[tl['pyramid_complex'] == 'Ibi', ['start_of_reign']] = 2143
+tl.loc[tl['pyramid_complex'] == 'Ibi', ['end_of_reign']] = 2141
+
+tl = tl[['pyramid_owner', 'start_of_reign', 'end_of_reign', 'length_of_reign']].dropna()
+pyramid_owners = tl['pyramid_owner']
+starts = tl['start_of_reign']
+ends = tl['end_of_reign']
+length = tl['length_of_reign']
+
+# Figure
+plt.figure(figsize=(16, 8))
+#color = cm.rainbow(np.linspace(0, 1, len(starts)))
+plt.barh(y=0, 
+         width=(ends - starts), 
+         height=0.3, 
+         left=starts, 
+         #color=color, 
+         color='none',
+         edgecolor='black')
+# Tick settings
+plt.tick_params(left=False, labelleft=False)
+plt.gca().invert_xaxis()
+plt.gca().xaxis.set_minor_locator(plt.MultipleLocator(10))
+# Labeling
+plt.xlabel('Year (BCE)')
+plt.ylabel('Ruler')
+plt.title('Reign of Ruler')
+# Vertical figure label lines
+# Kept in a hacky attempt at maintaining vertical width of timeline,
+# remove alpha variable if label lines are desired
+levels = np.tile([-2, 2, -1, 1], 
+                 int(np.ceil(len(pyramid_owners) / 4)))[:len(pyramid_owners)]
+plt.vlines(starts - length/2, 0, levels/2, color='black', alpha=0)
+
+# Figure label text (removed for template version)
+'''
+for i in range(len(starts)):
+    plt.text(starts.iloc[i] - length.iloc[i]/2, 
+             (levels[i]*1.05)/2, 
+             pyramid_owners.iloc[i], 
+             ha='center', 
+             fontsize = '7')
+'''
+
+plt.savefig('images/jose_timeline.png')
