057c3ae27e6c6ea1ea32d0a492abc5482fc4b9a0
pneumatick <55963487+pneumatick@users.noreply.github.com>
2024-08-22 11:33:53 -0400

    Consolidate functionally distinct portions of the queen scatterplot script into functions

--- src/plots/queens_height_scatter.py
+++ src/plots/queens_height_scatter.py
@@ -4,116 +4,142 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
-'''
-    Dataframe creation/manipulation
-'''
-
-# Import the dataset
-df = pd.DataFrame(pd.read_csv("assets/normalized_pyramid_data.csv"))
-
-# Drop pyramids with no known complex
-key = ['unknown', 'pyramid?']
-complexes = df[~df['pyramid_complex'].isin(key)]
-
-'''
-Fill in 'start_of_reign' for every row where it is NA with the year of the 
-respective King's start of reign.
-
-TODO: Add this functionality to the cleanup script.
-'''
-unique_comp = complexes['pyramid_complex'].unique()
-temp = df
-
-for comp in unique_comp:
-    start = temp[temp['pyramid_complex'] == comp]['start_of_reign'].max()
-    temp[temp['pyramid_complex'] == comp]['start_of_reign'].replace(np.nan, 
-                                                                    start, 
-                                                                    inplace=True)
-
-# This had to be done to get it in the correct order (value was missing)
-temp.loc[temp['pyramid_complex'] == 'Sneferu 3', 'start_of_reign'] = 2574
-
-# Drop rows with no pyramid height value
-temp.dropna(subset='height', inplace=True)
-# Sort all remaining rows in chronological order
-temp.sort_values(by='start_of_reign', ascending=False, inplace=True)
-
-''' 
-Get the height column to be interpreted as numeric by taking the average of 
-values containing two height estimates, or by using the projected heights 
-as opposed to actual heights, as directed by Christelle during a meeting.
-
-Input: String, int, or float representing a pyramid dimension value
-Output: Numeric type (likely float, possibly int) representing the pyramid 
-        dimension value
-'''
 def average_of_two(val):
+    """
+    Get the height column to be interpreted as numeric by taking the average 
+    of values containing two height estimates, or by using the projected 
+    heights as opposed to actual heights, as directed by Christelle during 
+    a meeting.
+
+    Input: 
+        val: String, int, or float representing a pyramid dimension value
+    Output: 
+        Numeric type (likely float, possibly int) representing the pyramid 
+        dimension value
+    """
     if isinstance(val, int) or isinstance(val, float) or pd.isna(val): 
         return val
 
-    if ',' in val: return 72    # Temporary: Deals with that one weird value
+    if ',' in val: 
+        return 72    # Temporary: Deals with that one weird value
 
     nums = val.split('-')
-    if len(nums) == 1: return float(nums[0])
+    if len(nums) == 1: 
+        return float(nums[0])
+    
     return (float(nums[0]) + float(nums[1])) / 2
 
-temp['height'] = temp['height'].map(average_of_two).astype(float)
+def prepare_dataframe(df):
+    """
+    Prepare a subset of the original dataframe to allow for the creation of 
+    our desired plot.
 
-# Create a new dataframe with a subset of data that is needed for the plot
-queens = temp[temp['royal_status'] == 'Queen']
-queen_data = queens[['pyramid_owner', 'dynasty', 'royal_status', 'daughter_of', 
-                     'royal_mother_title', 'likely_wife', 'wife_title', 
-                     'vizier', 'regent', 'relationship_to_king', 'height', 
-                     'title']]
-queen_data['dynasty'] = queen_data['dynasty'].astype(int)
+    Input:
+        df: Pandas dataframe containing the original normalized pyramid data
+    Output:
+        Pandas dataframe representing a subset of the original dataframe with 
+        additional modifications to allow for proper plotting
+    """
 
-'''
-Reshape queen data from wide to long (Binary categories get put into a new 
-column, each category applied to a specific queen given a row, with the status 
-of that category in another column)
-'''
-melted_queens = queen_data.melt(ignore_index=False, 
-                                id_vars=['dynasty', 'height', 'pyramid_owner', 
-                                         'relationship_to_king', 
-                                         'daughter_of', 'title'], 
-                                value_vars=['vizier', 'regent', 
-                                            'royal_mother_title', 
-                                            'likely_wife', 
-                                            'wife_title']).reset_index()
-# Select only those rows that correspond to some category applying to a given 
-# queen.
-melted_truth = melted_queens[melted_queens['value'] == True]
+    # Drop pyramids with no known complex
+    bad_pyramid_complexes = ['unknown', 'pyramid?']
+    complexes = df[~df['pyramid_complex'].isin(bad_pyramid_complexes)]
 
-''' 
-    Plotly portion
-'''
+    # Fill in 'start_of_reign' for every row where it is NA with the year of the 
+    # respective King's start of reign (ie max value of 'start_of_reign' for 
+    # that complex).
+    #
+    # TODO: Add this functionality to the cleanup script (?)
+    unique_comp = complexes['pyramid_complex'].unique()
+    temp = df
+    for comp in unique_comp:
+        start = temp[temp['pyramid_complex'] == comp]['start_of_reign'].max()
+        temp[temp['pyramid_complex'] == comp]['start_of_reign'].replace(np.nan, 
+                                                                        start, 
+                                                                        inplace=True)
 
-fig = px.strip(
-    melted_truth,
-    x = 'title',
-    y = 'height',
-    color = 'variable',
-    width = 1200,
-    title = 'Attributes of Pyramid-owning Queens',
-    labels = {
-        'title': 'Queen Pyramid',
-        'height': 'Height of Pyramid (meters)',
-        'variable': 'Attributes',
-    }
-)
-fig.update_layout(
-    title_x = 0.5,
-    xaxis = dict(
-        ticks = 'inside'
-    ))
+    # This had to be done to get it in the correct order (value was missing)
+    temp.loc[temp['pyramid_complex'] == 'Sneferu 3', 'start_of_reign'] = 2574
 
-# Update the legend variable names
-new_names = {'vizier': 'Vizier',
-             'regent': 'Regent',
-             'royal_mother_title': 'Royal Mother Title',
-             'likely_wife': 'Likely Wife',
-             'wife_title': 'Wife Title'}
-fig.for_each_trace(lambda t: t.update(name = new_names[t.name]))
+    # Drop rows with no pyramid height value
+    temp.dropna(subset='height', inplace=True)
 
-# Output the plot as a png image
-fig.write_image("images/jose-queen-height-by-status-scatter.png")
+    # Sort all remaining rows in chronological order
+    temp.sort_values(by='start_of_reign', ascending=False, inplace=True)
+
+    # Take the averages for elements with two height estimates
+    temp['height'] = temp['height'].map(average_of_two).astype(float)
+
+    # Create a new dataframe with a subset of data that is needed for the plot
+    columns = ['pyramid_owner', 'dynasty', 'royal_status', 'daughter_of', 
+                'royal_mother_title', 'likely_wife', 'wife_title', 'vizier', 
+                'regent', 'relationship_to_king', 'height', 'title']
+    # Create a new dataframe with a subset of data that is needed for the plot
+    queens = temp[temp['royal_status'] == 'Queen']
+    queen_data = queens[columns]
+    queen_data['dynasty'] = queen_data['dynasty'].astype(int)
+
+    '''
+    Reshape queen data from wide to long (Binary categories get put into a new 
+    column, each category applied to a specific queen given a row, with the status 
+    of that category in another column)
+    '''
+    melted_queens = queen_data.melt(ignore_index=False, 
+                                    id_vars=['dynasty', 'height', 'pyramid_owner', 
+                                            'relationship_to_king', 
+                                            'daughter_of', 'title'], 
+                                    value_vars=['vizier', 'regent', 
+                                                'royal_mother_title', 
+                                                'likely_wife', 
+                                                'wife_title']).reset_index()
+    # Select only those rows that correspond to some category applying to a given 
+    # queen.
+    melted_truth = melted_queens[melted_queens['value'] == True]
+
+    return melted_truth
+
+def create_figure(melted_truth):
+    fig = px.strip(
+        melted_truth,
+        x = 'title',
+        y = 'height',
+        color = 'variable',
+        width = 1200,
+        title = 'Attributes of Pyramid-owning Queens',
+        labels = {
+            'title': 'Queen Pyramid',
+            'height': 'Height of Pyramid (meters)',
+            'variable': 'Attributes',
+        }
+    )
+    fig.update_layout(
+        title_x = 0.5,
+        xaxis = dict(
+            ticks = 'inside'
+        ))
+
+    # Update the legend variable names
+    new_names = {'vizier': 'Vizier',
+                'regent': 'Regent',
+                'royal_mother_title': 'Royal Mother Title',
+                'likely_wife': 'Likely Wife',
+                'wife_title': 'Wife Title'}
+    fig.for_each_trace(lambda t: t.update(name = new_names[t.name]))
+
+    return fig
+
+def main():
+    # Import the dataset
+    df = pd.DataFrame(pd.read_csv("assets/normalized_pyramid_data.csv"))
+
+    # Prepare the dataframe for plotting
+    melted_truth = prepare_dataframe(df)
+
+    # Create the scatterplot
+    fig = create_figure(melted_truth)
+
+    # Output the plot as a png image
+    fig.write_image("images/jose-queen-height-by-status-scatter.png")
+
+if __name__ == "__main__":
+    main()
