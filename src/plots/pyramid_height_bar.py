b4c2f1dbcb7e806fc52792ef06b547f2113c9027
pneumatick <55963487+pneumatick@users.noreply.github.com>
2024-08-22 10:04:45 -0400

    Encapsulate functionally different aspect of the pyramid script in functions

--- src/plots/pyramid_height_bar.py
+++ src/plots/pyramid_height_bar.py
@@ -4,36 +4,6 @@ import pandas as pd
 import numpy as np
 from plotly import graph_objects as go
 
-# Dataframe creation/manipulation
-
-# Import the pyramid dataset
-df = pd.DataFrame(pd.read_csv("assets/normalized_pyramid_data.csv"))
-
-# Drop pyramids with no known complex
-bad_pyramid_complexes = ['unknown', 'pyramid?']
-complexes = df[~df['pyramid_complex'].isin(bad_pyramid_complexes)]
-
-# Fill in 'start_of_reign' for every row where it is NA with the year of the 
-# respective King's start of reign (ie max value of 'start_of_reign' for 
-# that complex).
-#
-# TODO: Add this functionality to the cleanup script (?)
-unique_comp = complexes['pyramid_complex'].unique()
-temp = df
-for comp in unique_comp:
-    start = temp[temp['pyramid_complex'] == comp]['start_of_reign'].max()
-    temp[temp['pyramid_complex'] == comp]['start_of_reign'].replace(np.nan, 
-                                                                    start, 
-                                                                    inplace=True)
-
-# This had to be done to get it in the correct order (value was missing)
-temp.loc[temp['pyramid_complex'] == 'Sneferu 3', 'start_of_reign'] = 2574
-
-# Drop rows with no pyramid height value
-temp.dropna(subset='height', inplace=True)
-# Sort all remaining rows in chronological order
-temp.sort_values(by='start_of_reign', ascending=False, inplace=True)
-
 def average_of_two(val):
     """
     Get the height column to be interpreted as numeric by taking the average 
@@ -50,120 +20,162 @@ def average_of_two(val):
     if isinstance(val, int) or isinstance(val, float) or pd.isna(val): 
         return val
 
-    if ',' in val: return 72    # Temporary: Deals with that one weird value
+    if ',' in val: 
+        return 72    # Temporary: Deals with that one weird value
 
     nums = val.split('-')
-    if len(nums) == 1: return float(nums[0])
+    if len(nums) == 1: 
+        return float(nums[0])
+    
     return (float(nums[0]) + float(nums[1])) / 2
-temp['height'] = temp['height'].map(average_of_two).astype(float)
 
-# Create a new dataframe with a subset of data that is needed for the plot
-columns  = ['pyramid_complex', 'pyramid_owner', 'start_of_reign', 'end_of_reign',
-           'length_of_reign', 'height', 'royal_status', 'relationship_to_king',
-           'title', 'pyramid_texts', 'state_of_completion'] # add formatting
-tl = temp[columns]
-# Omit Khentkaus I (Queen, not at a King's complex)
-tl = tl.drop(tl[tl['pyramid_complex'] == 'Khentkaus I'].index)
+# Dataframe creation/manipulation
+def prepare_dataframe(df):
+    # Drop pyramids with no known complex
+    bad_pyramid_complexes = ['unknown', 'pyramid?']
+    complexes = df[~df['pyramid_complex'].isin(bad_pyramid_complexes)]
 
-# Plotly portion
+    # Fill in 'start_of_reign' for every row where it is NA with the year of the 
+    # respective King's start of reign (ie max value of 'start_of_reign' for 
+    # that complex).
+    #
+    # TODO: Add this functionality to the cleanup script (?)
+    unique_comp = complexes['pyramid_complex'].unique()
+    temp = df
+    for comp in unique_comp:
+        start = temp[temp['pyramid_complex'] == comp]['start_of_reign'].max()
+        temp[temp['pyramid_complex'] == comp]['start_of_reign'].replace(np.nan, 
+                                                                        start, 
+                                                                        inplace=True)
 
-# Create list of colors for the bars to indicate royal status
-def setColor(y):
-    if y == "King": return '#636EFA'     # default plotly blue
-    elif y == "Queen": return '#EF553B'  # default plotly red
-colors = [setColor(y) for y in tl['royal_status'].values]
+    # This had to be done to get it in the correct order (value was missing)
+    temp.loc[temp['pyramid_complex'] == 'Sneferu 3', 'start_of_reign'] = 2574
 
-# Graph objects histogram
-horizontal = go.Figure(go.Bar(
-    x = tl.loc[:,["pyramid_complex", "title"]].T.values, 
-    y = tl["height"].values, 
-    marker={"color": colors,
-            "pattern_shape": tl['state_of_completion'].map({'Unfinished': 'x', 
-                                                            'Unfinished?': '/', 
-                                                            'Completed': '', 
-                                                            '': '', 
-                                                            np.nan: ''}),
-            "line_color": tl['pyramid_texts'].map({'Yes': 'black', 
-                                                   np.nan: 'white'}),
-            "line_width": tl['pyramid_texts'].map({'Yes': 2.5, np.nan: 0.5})},
-    name = "King",
-    legendgroup='royal',
-    customdata = np.stack((tl['pyramid_owner'], 
-                           tl['relationship_to_king'].fillna('Self'), 
-                           tl['pyramid_texts'].fillna('No'),
-                           tl['state_of_completion'].fillna('Completed')), 
-                           axis=-1),
-    hovertemplate='<br>'.join([
-        'Pyramid Complex: %{x[0]}',
-        'State of Completion: %{customdata[3]}',
-        'Height: %{y} m',
-        'Pyramid Owner: %{customdata[0]}',
-        'Relationship To King: %{customdata[1]}',
-        'Pyramid Texts?: %{customdata[2]}'
-        '<extra></extra>'
-    ])))
+    # Drop rows with no pyramid height value
+    temp.dropna(subset='height', inplace=True)
+    # Sort all remaining rows in chronological order
+    temp.sort_values(by='start_of_reign', ascending=False, inplace=True)
 
-# Create dummy traces for the legend because plotly wouldn't do it automatically
-horizontal.add_trace(
-    go.Bar(
-    x=["Dummy"],
-    y=[0],
-    name="Queen",
-    legendgroup='royal'))
-horizontal.add_trace(
-    go.Bar(
-        x=[None],
-        y=[None],
-        name='Unfinished',
-        legendgroup='completion',
-        marker_color = 'white',
-        marker_pattern_shape = 'x',
-        marker_line_width = 0
+    temp['height'] = temp['height'].map(average_of_two).astype(float)
+
+    # Create a new dataframe with a subset of data that is needed for the plot
+    columns  = ['pyramid_complex', 'pyramid_owner', 'start_of_reign', 'end_of_reign',
+            'length_of_reign', 'height', 'royal_status', 'relationship_to_king',
+            'title', 'pyramid_texts', 'state_of_completion'] # add formatting
+    tl = temp[columns]
+    # Omit Khentkaus I (Queen, not at a King's complex)
+    tl = tl.drop(tl[tl['pyramid_complex'] == 'Khentkaus I'].index)
+
+    return tl
+
+def create_figure(tl):
+    # Graph objects histogram
+    horizontal = go.Figure(go.Bar(
+        x = tl.loc[:,["pyramid_complex", "title"]].T.values, 
+        y = tl["height"].values, 
+        marker={"color": tl['royal_status'].map({'King': '#636EFA', 
+                                                 'Queen': '#EF553B'}),
+                "pattern_shape": tl['state_of_completion'].map({'Unfinished': 'x', 
+                                                                'Unfinished?': '/', 
+                                                                'Completed': '', 
+                                                                '': '', 
+                                                                np.nan: ''}),
+                "line_color": tl['pyramid_texts'].map({'Yes': 'black', 
+                                                    np.nan: 'white'}),
+                "line_width": tl['pyramid_texts'].map({'Yes': 2.5, 
+                                                       np.nan: 0.5})},
+        name = "King",
+        legendgroup='royal',
+        customdata = np.stack((tl['pyramid_owner'], 
+                            tl['relationship_to_king'].fillna('Self'), 
+                            tl['pyramid_texts'].fillna('No'),
+                            tl['state_of_completion'].fillna('Completed')), 
+                            axis=-1),
+        hovertemplate='<br>'.join([
+            'Pyramid Complex: %{x[0]}',
+            'State of Completion: %{customdata[3]}',
+            'Height: %{y} m',
+            'Pyramid Owner: %{customdata[0]}',
+            'Relationship To King: %{customdata[1]}',
+            'Pyramid Texts?: %{customdata[2]}'
+            '<extra></extra>'
+        ])))
+
+    # Create dummy traces for the legend because plotly wouldn't do it automatically
+    horizontal.add_trace(
+        go.Bar(
+        x=["Dummy"],
+        y=[0],
+        name="Queen",
+        legendgroup='royal'))
+    horizontal.add_trace(
+        go.Bar(
+            x=[None],
+            y=[None],
+            name='Unfinished',
+            legendgroup='completion',
+            marker_color = 'white',
+            marker_pattern_shape = 'x',
+            marker_line_width = 0
+        )
     )
-)
-horizontal.add_trace(
-    go.Bar(
-        x=[None],
-        y=[None],
-        name='Unfinished?',
-        legendgroup='completion',
-        marker_color = 'white',
-        marker_pattern_shape = '/',
-        marker_line_width = 0
+    horizontal.add_trace(
+        go.Bar(
+            x=[None],
+            y=[None],
+            name='Unfinished?',
+            legendgroup='completion',
+            marker_color = 'white',
+            marker_pattern_shape = '/',
+            marker_line_width = 0
+        )
     )
-)
-horizontal.add_trace(
-    go.Bar(
-        x=[None],
-        y=[None],
-        name='Pyramid Texts',
-        legendgroup='texts',
-        marker_color='white',
-        marker_line_color='black',
-        marker_line_width=2.5
+    horizontal.add_trace(
+        go.Bar(
+            x=[None],
+            y=[None],
+            name='Pyramid Texts',
+            legendgroup='texts',
+            marker_color='white',
+            marker_line_color='black',
+            marker_line_width=2.5
+        )
     )
-)
 
-# Final plot adjustments
-horizontal.update_layout(
-    title = "Height of Pyramids By Complex",
-    xaxis = dict(title = "Pyramid Owner (Grouped By Complex)", 
-                 dividercolor='#e8e8e8'),
-    yaxis = dict(
-        title = "Height (meters)", 
-        dtick = 10),
-    showlegend = True,
-    width = 1200,
-    height = 800,
-    bargap = 0.15,
-    bargroupgap = 0,
-    legend = dict(
-        yanchor = "top",
-        y = 1.2,
-        xanchor = "left",
-        x = 0.4,
-        orientation='h'
-    ))
+    # Final plot adjustments
+    horizontal.update_layout(
+        title = "Height of Pyramids By Complex",
+        xaxis = dict(title = "Pyramid Owner (Grouped By Complex)", 
+                    dividercolor='#e8e8e8'),
+        yaxis = dict(
+            title = "Height (meters)", 
+            dtick = 10),
+        showlegend = True,
+        width = 1200,
+        height = 800,
+        bargap = 0.15,
+        bargroupgap = 0,
+        legend = dict(
+            yanchor = "top",
+            y = 1.2,
+            xanchor = "left",
+            x = 0.4,
+            orientation='h'
+        ))
+    
+    return horizontal
 
-# Output plot as a png image
-horizontal.write_image('images/jose-total-complex-height-by-status-bar.png')
+def main():
+    # Import the pyramid dataset
+    df = pd.DataFrame(pd.read_csv("assets/normalized_pyramid_data.csv"))
+    tl = prepare_dataframe(df)
+
+
+    # Plotly portion
+    horizontal = create_figure(tl)
+
+    # Output plot as a png image
+    horizontal.write_image('images/jose-total-complex-height-by-status-bar.png')
+
+if __name__ == "__main__":
+    main()
